f3d3b63a0410b6d8c4cf3613e5bcd0d8d65234e6
Chris <chris.njuguna@gmail.com>
2016-07-14 18:50:56 +0300

    Updated codebook.md redone script and renamed and sorted output file

--- assignment4.R
+++ assignment4.R
@@ -6,14 +6,27 @@
 
 library("dplyr") # load the dplyr library for easier manipulation of data
 
+
 #load the train data
 trainx <- read.table("./train/X_train.txt", sep = "",header = FALSE)
+
+activity <- read.table("./train/y_train.txt", sep = "",header = FALSE)
+
+trainx <- cbind(activity, trainx)
+
+rm(activity)
 
 #load the test data
 testx <- read.table("./test/X_test.txt", sep = "",header = FALSE)
 
+activity <- read.table("./test/y_test.txt", sep = "",header = FALSE)
+
+testx <- cbind(activity,testx)
+
+rm(activity)
+
 # load the column names
-colNames <- read.csv("features.txt", sep=" ",header = F)
+colNames <- read.csv("features.txt", sep=" ",header = F, stringsAsFactors = F)
 
 #load the activity name mapping
 activities <- read.csv("activity_labels.txt", sep=" ",header = F)
@@ -32,9 +45,8 @@ testx <- cbind(subject_test, testx)
 #we remove the numbering column from the column names which is not required
 colNames <- colNames[,2]
 
-#replace all activity numbers in the column names with their mapped names
-for (i in 1:6)
-  {colNames <- sub(paste0("[)|,]", activities[i,1], "$"), activities[i,2], colNames)}
+#add the activity column name
+colNames <- c("activity", as.character(colNames))
 
 #The names are in a mess. Assign clean unique names
 colNames <- make.names(colNames, unique=TRUE, allow_ = TRUE)
@@ -46,21 +58,22 @@ dat <- rbind(trainx, testx)
 dat <- tbl_df(dat)
 
 #name the data column names with the names from the columns data
+#check the name of the subject column
+colNames <- c("subject", colNames)
 names(dat) <- colNames
 
 # select only fields relating to mean or standard deviation
 # i.e. that have mean or std in them
-dtMeanStd <- dplyr::select(dat, matches("mean|std"))
+dtMeanStd <- dplyr::select(dat, subject, activity, matches("mean|std"))
 
-#check the name of the subject column
-names(dtMeanStd)[1] <- "subject"
-  
 #Calculate averages for each variable for each activity and each subject
-dtSubj <- split(dtMeanStd, dtMeanStd$subject)
+dtSubj <- split(dtMeanStd, list(dtMeanStd$subject, dtMeanStd$activity))
 
 #calculate the column means for each subject
 dtSubjMeans <- sapply(dtSubj, colMeans)
 
 dtSubjMeans <- t(dtSubjMeans)
 
-write.table(dtSubjMeans, "output.csv", row.names = F, sep = ",")
+dtSubjMeans <- dtSubjMeans[order(dtSubjMeans[,1], dtSubjMeans[,2]), ]
+
+write.table(dtSubjMeans, "output.txt", row.names = F, sep = ",")
